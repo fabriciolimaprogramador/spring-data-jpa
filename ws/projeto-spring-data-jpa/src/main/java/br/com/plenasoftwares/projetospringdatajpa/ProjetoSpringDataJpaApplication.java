5d562fe0f6912f1732b3ea1c5e730fff831486ff
Fabrício Lima <fabriciolimaprogramador@gmail.com>
2022-02-15 16:14:50 -0300

    Spŕing Data JPA
    Spŕing Data JPA

---
+++ ws/projeto-spring-data-jpa/src/main/java/br/com/plenasoftwares/projetospringdatajpa/ProjetoSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.plenasoftwares.projetospringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringDataJpaApplication.class, args);
+	}
+
+}
